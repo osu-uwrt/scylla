9a67676111b6e9c7b75d7ab4b15efb28f7ef2105
Anden Acitelli <a.acitelli@gmail.com>
2019-11-17 11:42:57 -0500

    Implemented feeding local files into OpenLabeling

--- renderer.js
+++ renderer.js
@@ -25,8 +25,7 @@ function launchOpenLabeling() {
 // This method copies over all the passed-in files to 
 // OpenLabeling's startup file 
 // To make sure nothing is accidentally deleted, this will 
-// copy the files into a folder called backup/ in the 
-moveFilesToOpenLabeling();
+// copy the files into a folder called backup/ in the
 function moveFilesToOpenLabeling() {   
 
   console.log.bind(console.log);
@@ -37,10 +36,10 @@ function moveFilesToOpenLabeling() {
   const ncp = require("ncp").ncp; // Required for folder copying 
   const rimraf = require("rimraf"); // Required for recursive folder deletion
 
-  // * "Backing up" all files in the current input directory so they aren't permanently lost. This is here incase the user needs those files. 
+  // * Back Up, then delete all files currently in the '/input' directory 
   // Reading in the contents of that directory 
   console.debug("Backing up all files currently in '/input' to '/filebackup'.");
-  const inputDirPath = path.join(__dirname, "/OpenLabeling/main/input");
+  const inputDirPath = path.join(__dirname, "../OpenLabeling/main/input");
   fs.readdir(inputDirPath, (err, files) => {
     
     if (err) { 
@@ -83,26 +82,27 @@ function moveFilesToOpenLabeling() {
           if (err) { return console.error("Unable to delete file in /input: " + err); }
         })
       } */
-    }
-
-    // TODO: Copy all files that were selected into the input directory L
-    // Each element in here has a .name and a .path (absolute) property that 
-    // we can use to move it via the fs module 
-    //files = document.getElementById("localFileInput").files; 
-    for (let i = 0; i < files.length; i++) {
-
-      // Debug Output 
-      console.debug("(" + (i + 1) + "/" + files.length + ") Copying file/folder " + files[i] + " to '/input'.");
-
-      ncp(path.join(files[i].path, files[i].name), path.join(__dirname + "/input", files[i]), (err) => {
-        if (err) { 
-          console.error("Unable to copy file from /input to /filebackup: " + err);
-          return;
-        }
-      })
-
-    }
+    }    
   });   
+
+  // * Copy our selected files into OpenLabeling's input directory 
+  // Each element in here has a .name and a .path (absolute) property that 
+  // we can use to move it via the fs module 
+  files = document.getElementById("localFileInput").files; 
+  for (let i = 0; i < files.length; i++) {
+
+    // Debug Output 
+    console.debug("(" + (i + 1) + "/" + files.length + ") Copying file/folder " + files[i] + " to '/input'.");
+    console.log(JSON.stringify(files[i].path));
+
+    ncp(files[i].path, path.join(__dirname + "/../OpenLabeling/main/input", files[i].name), (err) => {
+      if (err) { 
+        console.error("Unable to copy file from /input to /filebackup: " + err);
+        return;
+      }
+    })
+
+  }
 }
 
 // TODO: Implement this (I don't have an exact idea how this works, so you'll need to do some research, but it'll take a bit of time to understand and implement)
