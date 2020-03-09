e09d72d03473d3f085b9fe5bc639093500573111
Anden Acitelli <a.acitelli@gmail.com>
2020-03-09 12:54:52 -0400

    Misc. minor changes; Now works

--- setup/setupPythonPrereqs.bash
+++ setup/setupPythonPrereqs.bash
@@ -2,4 +2,5 @@
 # -r tells pip to install from the requirements.txt file 
 # -t (short for target) specifies the directory that everything will be installed to 
 # --no-cache-dir tells pip to always reinstall to this specific directory and don't use global or user copies of the software (useful to ensure bundling works correctly)
-../extraResources/Python3.6.10/python -mpip install  -r requirements.txt -t ../extraResources/site_packages --no-cache-dir
+sudo apt-get install python3-pip -y
+sudo python3 -mpip install -r requirements.txt -t ../extraResources/site_packages --no-cache-dir
